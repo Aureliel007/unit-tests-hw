2e4108ff124ce673061a01732b7aa52db212eaf7
Natalia Gribanova <kara333@mail.ru>
2024-03-17 12:31:47 +0300

    Added selenium login function

--- task_2/test_yadisk.py
+++ task_2/test_yadisk.py
@@ -1,33 +1,30 @@
-# from configparser import ConfigParser
+from configparser import ConfigParser
 
-# import pytest
+import pytest
 
-# from yadisk import YaDiskNewFolder
-
-
-# class TestYaDiskNewFolder:
-
-#     def setup_method(self):
-#         settings = ConfigParser()
-#         settings.read('settings.ini')
-#         token = settings['Yadisk']['token']
-#         self.yadisk = YaDiskNewFolder(token)
-
-#     def teardown_method(self):
-#         del self.yadisk
-
-#     def test_create_folder(self):
-#         response = self.yadisk.create_folder('one_more_folder')
-#         assert response.status_code == 201
-
-#     def test_folder_exists(self):
-#         self.yadisk.create_folder('folder_exists')
-#         response = self.yadisk.create_folder('folder_exists')
-#         assert response.status_code == 409
-
-#     def test_wrong_path(self):
-#         response = self.yadisk.wrong_path()
-#         assert response.status_code == 404
+from yadisk import YaDiskNewFolder
 
 
-    
+class TestYaDiskNewFolder:
+
+    def setup_method(self):
+        settings = ConfigParser()
+        settings.read('settings.ini')
+        token = settings['Yadisk']['token']
+        self.yadisk = YaDiskNewFolder(token)
+
+    def teardown_method(self):
+        del self.yadisk
+
+    def test_create_folder(self):
+        response = self.yadisk.create_folder('one_more_folder')
+        assert response.status_code == 201
+
+    def test_folder_exists(self):
+        self.yadisk.create_folder('folder_exists')
+        response = self.yadisk.create_folder('folder_exists')
+        assert response.status_code == 409
+
+    def test_wrong_path(self):
+        response = self.yadisk.wrong_path()
+        assert response.status_code == 404
--- task_3_selenium.py
+++ task_3_selenium.py
@@ -1,1 +1,49 @@
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+
+import time
+
+
+def yandex_auth():
+    chrome_path = ChromeDriverManager().install()
+    browser_service = Service(executable_path=chrome_path)
+    o = Options()
+    o.add_experimental_option("detach", True)
+    browser = Chrome(service=browser_service, options=o)
+
+    browser.get('https://passport.yandex.ru/auth/')
+    login = browser.find_element(by=By.ID, value='passp-field-login')
+    login.click()
+    login.send_keys('avtorus.serwis')
+    browser.find_element(by=By.ID, value='passp:sign-in').click()
+    time.sleep(1)
+
+    try:
+        login_err = browser.find_element(by=By.ID, value='field:input-login:hint')
+        return login_err.text.strip()
+    except:
+        print('Логин введел')
+
+    passw = browser.find_element(by=By.ID, value='passp-field-passwd')
+    passw.click()
+    passw.send_keys('214365qwe!')
+    browser.find_element(by=By.ID, value='passp:sign-in').click()
+    time.sleep(1)
+
+    try:
+        passw_err = browser.find_element(by=By.ID, value='field:input-passwd:hint')
+        return passw_err.text.strip()
+    except:
+        print('Пароль введен')
+
+    result = browser.current_url
+    browser.close()
+    return result
+
+
+    
+print(yandex_auth())
+
